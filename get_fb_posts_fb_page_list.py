d16e71b2069c40f6937e4e4568dcd9268ed6d84d
0x <csa5040@gmail.com>
2016-10-24 02:39:12 -0400

    Scraper now also makes modifications to the posts
    Hashtags, hyperlinks, whitespace, and emoticons are handled by either
    removal or replacement. Post length is limited by default to 40
    tokens, and a command line switch has been added to change this.
    Page name and a hard-coded industry have been added to the output.

--- get_fb_posts_fb_page_list.py
+++ get_fb_posts_fb_page_list.py
@@ -4,11 +4,24 @@ import datetime
 import csv
 import time
 import argparse
+import re
 
 app_id = "<FILL IN>"
 app_secret = "<FILL IN>" # DO NOT SHARE WITH ANYONE!
 
 access_token = app_id + "|" + app_secret
+
+# regex for detecting smileys: see http://stackoverflow.com/questions/20582030/detect-emoticon-in-a-sentence-using-regex-python#20583383
+#emoji_regex = r'[:;=]+[-~]?[()DoOpP]+' # from Eclipse project
+eyes, noses, mouths = r":;8BX=", r"-~'^", r")(/\|DP"
+emoji_pattern = "[%s][%s]?[%s]" % tuple(map(re.escape, [eyes, noses, mouths]))
+
+smileys = """:-) :) :o) :] :3 :c) :> =] 8) =) :} :^) 
+             :D 8-D 8D x-D xD X-D XD =-D =D =-3 =3 B^D""".split()
+emoji_list = "|".join(map(re.escape, smileys))
+# regex for detecting a URL: see http://stackoverflow.com/questions/700163/detecting-a-naughty-or-nice-url-or-link-in-a-text-string
+url_pattern = r'[^\b]\.([a-zA-Z]{2}|aero|asia|biz|cat|com|coop|edu|gov|info|int|jobs|mil|mobi|museum|name|net|org|pro|tel|travel)[\b/]'
+whitespace_pattern = r'[\n\t ]+'
 
 def request_once(url):
     req = urllib2.Request(url)
@@ -140,7 +153,31 @@ def processFacebookPageFeedStatus(status, access_token):
             "status_published":status_published, "num_reactions":num_reactions, "num_comments":num_comments, "num_shares":num_shares,
             "num_likes":num_likes, "num_loves":num_loves, "num_wows":num_wows, "num_hahas":num_hahas, "num_sads":num_sads, "num_angrys":num_angrys}
 
-def scrapeFacebookPageFeedStatus(page_id, access_token):
+
+def prepPost(post_message, max_tokens):
+    if not post_message:
+        return False
+    # remove anything that looks like a URL -- do this BEFORE emoji matching, because they conflict!
+    post_message = re.sub(url_pattern, "HTTP-REF", post_message)
+    # remove specific emojis
+    post_message = re.sub(emoji_list, "", post_message)
+    # try to remove remaining emojis based on a pattern
+    post_message = re.sub(emoji_pattern, "", post_message) # note, subn can be used for debugging/advanced usage to detect if anything is actually matched
+    # remove excess whitespace
+    post_message = re.sub(r'\s+', " ", post_message)
+    # remove hashtags
+    post_message = re.sub(r"#[a-zA-Z0-9]+", "HASHTAG", post_message)
+    # remove leading and trailing whitespace
+    post_message = post_message.strip()
+    # make sure the number of tokens fits our guidelines
+    if len(post_message.split()) > max_tokens:
+        return False
+    # if we still have a message after all of this, add it to the output
+    if not post_message:
+        return False
+    return post_message
+
+def scrapeFacebookPageFeedStatus(page_id, access_token, max_tokens):
 
     has_next_page = True
     num_processed = 0   # keep a count on how many we've processed
@@ -152,41 +189,46 @@ def scrapeFacebookPageFeedStatus(page_id, access_token):
     if not statuses:
         print "\nFailed! No posts scraped from this page.\n"
         print "--------------------------------------------------------------"
-        return
+        return False
 
     filename='%s_facebook_statuses_%s.json' %(page_id, datetime.datetime.now().strftime('%Y-%m-%d'))
     with open(filename, 'wb') as file:
 
-        file.write('[')
+        file.write('{"posts":[')
         while has_next_page:
             for status in statuses['data']:
 
                 # Ensure it is a status with the expected metadata
                 if 'reactions' in status:
-                    if num_processed != 0:
-                        file.write(",\n")
                     processed_status = processFacebookPageFeedStatus(status, access_token)
-                    file.write(json.dumps({ "id":processed_status["status_id"],
-                                            "created_time":processed_status["status_published"],
-                                            "message":processed_status["status_message"],
-                                            "type":status['type'],
-                                            "link_name":processed_status["link_name"],
-                                            "likes":processed_status["num_likes"],
-                                            "loves":processed_status["num_loves"],
-                                            "wows":processed_status["num_wows"],
-                                            "hahas":processed_status["num_hahas"],
-                                            "sads":processed_status["num_sads"],
-                                            "angrys":processed_status["num_angrys"],
-                                            "comments":processed_status["num_comments"],
-                                            "shares":processed_status["num_shares"]
-                                          }, file, indent=4))
+                    # do not add statuses with empty messages (e.g. photos)
+                    post_message = prepPost(processed_status['status_message'], max_tokens)
+                    if post_message:
+                        if num_processed != 0:
+                            file.write(",\n")
+                        file.write(json.dumps({ "company":page_id,
+                                                "industry":"fast food",
+                                                "id":processed_status["status_id"],
+                                                "created_time":processed_status["status_published"],
+                                                "message":post_message,
+                                                "type":status['type'],
+                                                "link_name":processed_status["link_name"],
+                                                "likes":processed_status["num_likes"],
+                                                "loves":processed_status["num_loves"],
+                                                "wows":processed_status["num_wows"],
+                                                "hahas":processed_status["num_hahas"],
+                                                "sads":processed_status["num_sads"],
+                                                "angrys":processed_status["num_angrys"],
+                                                "comments":processed_status["num_comments"],
+                                                "shares":processed_status["num_shares"]
+                                              }, file))
 
-                # output progress occasionally to make sure code is not
-                # stalling
-                num_processed += 1
-                if num_processed % 1000 == 0:
-                    print "%s Statuses Processed: %s" % \
-                        (num_processed, datetime.datetime.now())
+                        # output progress occasionally to make sure code is not
+                        # stalling
+                        num_processed += 1
+                        if num_processed % 1000 == 0:
+                            print "%s Statuses Processed: %s" % \
+                                (num_processed, datetime.datetime.now())
 
             # if there is no next page, we're done.
             has_next_page = False
@@ -197,24 +239,42 @@ def scrapeFacebookPageFeedStatus(page_id, access_token):
                     if statuses != None:
                         has_next_page = True
 
-        file.write("]\n")
+        file.write("]}\n")
 
         print "\nDone!\n%s Statuses Processed in %s" % \
                 (num_processed, datetime.datetime.now() - scrape_starttime)
+
+        return True
 
 
 if __name__ == '__main__':
     # read in the input file containing a list of facebook pages from the command line
     parser = argparse.ArgumentParser(description="InputFile")
     parser.add_argument('--file', dest='input_file', required=True)
+    parser.add_argument('--maxtokens', '-T', dest='max_tokens', required=False, type=int, default=40)
     args = parser.parse_args()
     
+    scrape_starttime = datetime.datetime.now()
+    num_processed = 0
+
+    skipped_pages = []
+
     #loop through the id list
     with open(args.input_file, 'rb') as input_file:
         for line in input_file.xreadlines():
             line=line.rstrip('\r\n')
             page_id=line
-            scrapeFacebookPageFeedStatus(page_id, access_token)
+            if scrapeFacebookPageFeedStatus(page_id, access_token, args.max_tokens):
+                num_processed += 1
+            else:
+                skipped_pages.append(page_id)
 
+    print "\nDone!\n%s Pages Scraped in %s" % \
+            (num_processed, datetime.datetime.now() - scrape_starttime)
 
-# The CSV can be opened in all major statistical programs. Have fun! :)
+    if skipped_pages:
+        print "\nSkipped Pages:\n"
+        for skipped in skipped_pages:
+            print skipped,"\n"
+    else:
+        print "No Pages Skipped."
